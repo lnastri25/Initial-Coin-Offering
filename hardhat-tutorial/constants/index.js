dddb03a038ae20684128c27a4850375140e62653
Lorenzo <lorenzonastri@gmail.com>
2023-01-01 17:53:26 -0300

    ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xe31d3660003007e124C609A7B9fECe2c8da56348";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
